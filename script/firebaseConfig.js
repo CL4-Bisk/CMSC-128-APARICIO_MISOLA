c682408b1b99c86ea3460be637c137162aa1d436
CL4-Bisk <CL4-Bisk@users.noreply.github.com>
2025-12-09 03:36:36 +0000

    deploy: 837755bf2f91d822d7128844bfd78995dccd7ec4

---
+++ script/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBP-WDUknqqrYDeVP-NETfRNtXiYt6Z0mY',
+  authDomain: 'cmsc128-apariciomisola.firebaseapp.com',
+  projectId: 'cmsc128-apariciomisola',
+  storageBucket: 'cmsc128-apariciomisola.firebasestorage.app',
+  messagingSenderId: '622507766800',
+  appId: '1:622507766800:web:5bdc30bf220d3f856ac695'
+};
